bab77c5faf4cd1b467fcd0b8987b4654ad25890d
hardikitgithub <hardikvaghasiya804@gmail.com>
2022-12-27 16:45:29 +0530

    first change

---
+++ module3Q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name:Vaghasiya Hardik K.\n");
+    printf("Birth date:24-01-1999.\n");
+    printf("Age:23 Year.\n");
+    printf("Address:148-Aangan raw house,kathodara.\n");
+
+    return(0);
+}
---
+++ module3Q2.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
++    printf("1-addition 2-substraction 3-multiplication 4-division");
+    int ch;
+    printf("\n ENTER YOUR CHOICE:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+      case'+':
+         {
+            int a,b;
+             printf("ENTER TWO NUMBER:");
+             scanf("%d %d",&a,&b);
+             int answer=a+b;;
+             printf("answer=%d,answer");
+             break;
+        }
+    case'-':
+         {
+            int a,b;
+             printf("ENTER TWO NUMBER:");
+             scanf("%d %d",&a,&b);
+             int answer=a-b;;
+             printf("answer=%d,answer");
+             break;
+        }
+    case'*':
+         {
+            int a,b;
+             printf("ENTER TWO NUMBER:");
+             scanf("%d %d",&a,&b);
+             int answer=a*b;;
+             printf("answer=%d,answer");
+             break;
+        }
+    case'/':
+         {
+            int a,b;
+             printf("ENTER TWO NUMBER:");
+             scanf("%d %d",&a,&b);
+             int answer=a/b;;
+             printf("answer=%d,answer");
+             break;
+        }
+     default:
+        {
+            printf("PLEASE CHOOSE CORRECT OPTION FROM MENU:");
+        }
+    
+    
+    }
+    
+    return 0;
+}
